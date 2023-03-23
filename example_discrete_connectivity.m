47173902f781e02cafbb7c2c642bfe2df73b8e98
PPWangyc <ywang330@u.rochester.edu>
2023-03-23 01:09:45 -0400

    add

--- example_discrete_connectivity.m
+++ example_discrete_connectivity.m
@@ -4,11 +4,11 @@ close all;
 addpath(genpath('../SBCI_Toolkit'))
 
 % load example data
-load('./example_data/example_fc.mat')
-load('./example_data/example_sc.mat')
+load('/scratch/ywang330/SBCI_AVG/CogTE1001/dwi_pipeline/sbci_connectome/RUN001/fc_avg_ico4.mat')
+load('/scratch/ywang330/SBCI_AVG/CogTE1001/dwi_pipeline/sbci_connectome/smoothed_sc_avg_0.005_ico4.mat')
 
 % load required SBCI data for mapping and analysis
-[sbci_parc, sbci_mapping, adjacency] = load_sbci_data('./example_data/', 0.94);
+[sbci_parc, sbci_mapping, adjacency] = load_sbci_data('/scratch/ywang330/SBCI_AVG', 'ico4');
  
 % convert into full matrices (for plotting)
 fc = fc + fc' - 2*diag(diag(fc));
@@ -20,20 +20,20 @@ sc = sc + sc' - 2*diag(diag(sc));
 % We use 'parcellate_fc'/'parcellate_sc' to calculate discrete FC/SC. 
 % Note that here the fourth argument in these functions is 'roi_mask',
 % which is a vector of ROI labels to remove;
-
-my_ROI_list = sbci_parc(11).names; %ROI names
+sbci_parc.atlas
+my_ROI_list = sbci_parc(12).names; %ROI names
 
 %for Desikan, ROIs 1 and 36 are 'LH_missing' and 'RH_missing'. So let's remove them 
 my_ROI_list([1,36],:) = []; %updated ROI names
 
-dct_fc = parcellate_fc(fc, sbci_parc(11), sbci_mapping, 'roi_mask', [1,36]);
-dct_sc = parcellate_sc(sc, sbci_parc(11), sbci_mapping, 'roi_mask', [1,36]);
+dct_fc = parcellate_fc(fc, sbci_parc(12), sbci_mapping, 'roi_mask', [1,36]);
+dct_sc = parcellate_sc(sc, sbci_parc(12), sbci_mapping, 'roi_mask', [1,36]);
 
 %% Plot Continuous Connectivity
-plot_sbci_mat(fc, sbci_parc(11), 'roi_mask', [1,36], 'figid', 1, 'clim', [-0.1, 0.35]);
+plot_sbci_mat(fc, sbci_parc(12), 'roi_mask', [1,36], 'figid', 1, 'clim', [-0.1, 0.35]);
 title('Continuous FC (Desikan)')
 
-plot_sbci_mat(log((10^7*sc) + 1), sbci_parc(11), 'roi_mask', [1,36], 'figid', 2, 'clim', [0, 3.5]);
+plot_sbci_mat(log((10^7*sc) + 1), sbci_parc(12), 'roi_mask', [1,36], 'figid', 2, 'clim', [0, 3.5]);
 title('Continuous SC (Desikan)')
 
 %% Plot Discrete FC
